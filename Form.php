d0f0fcb447ff81701a0bfbc2dc70e42bfeaaa053
SabrinaAzad001 <sabrinaazad53@gmail.com>
2025-07-20 10:38:31 +0600

    Add files via upload

---
+++ Form.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+
+<title> A Registration Form</title>
+
+<body>
+<center>
+
+<h1 style = "color : blue;" > AIUB </h1>
+<h3 style = "color : blue;" > Registration Form </h3>
+
+
+</center>   
+
+<h3 align = "left"> Enter Your Information </h3>
+
+<table>
+<tr> 
+    <td> Full Name: </td>
+    <td><input type ="text"></td>
+
+</tr>
+<tr>
+
+
+    <td> Email:  </td>
+    <td><input type ="text"></td>
+    
+</tr>
+
+
+<tr>
+    <td>Password:</td>
+    <td><input type="password" name= "password" value= "" ></td>
+
+</tr>
+
+<tr>
+    <td> Gender: </td>
+    <td><input type = "radio" name ="des" value =""> Male </td>
+     <td><input type = "radio" name ="des" value =""> Other </td>
+
+</tr>
+
+<tr>
+    <td> Language Known: </td>
+    <td><input type = "radio" name ="des" value =""> English</td>
+    <td><input type = "radio" name ="des" value =""> Bangla</td>
+    <td><input type = "radio" name ="des" value =""> Hindi</td>
+
+</tr>
+
+<tr>
+    <td> Country: </td>
+    <td>
+        <select name= "Select">
+        <option value ="text" > Select</option>
+        </Select>
+   
+
+</tr>
+
+
+<tr>
+    <td> Date of Birth: </td>
+    <td> <input type ="Date" ></td>
+
+</tr>
+
+
+
+<tr>
+    <td> Upload Photos: </td>
+    <td> <input type ="file" name= "" value ="" ></td>
+
+
+</tr>
+
+
+<tr>
+    <td> Comments: </td>
+    <td> <input type ="text" ></td>
+
+</tr>
+
+
+<tr>
+    <td>  </td>
+    <td> <input type ="Submit" name= "" value="Submit" ></td>
+
+</tr>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+</html>
