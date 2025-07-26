27f2cf8cb104a046f470c8904e7627e68a9aaa9f
SabrinaAzad001 <sabrinaazad53@gmail.com>
2025-07-26 10:51:50 +0600

    Add files via upload

---
+++ Mid labtask/labtask_2.php
@@ -0,0 +1,142 @@
+<!DOCTYPE HTML>
+<HTML>
+
+<title> Bank Management System </title>
+
+<body>
+<center>
+    <h2 style = "color: blue;" > Bank Management System </h2>
+    <h3 style = "color : blue;" > Your Trusted Financial Partner </h3>
+</center>
+
+<h3 align = "left"> Customer Registration Form</h3>
+
+<table>
+<tr> 
+    <td> Full Name: </td>
+    <td><input type ="text"></td>
+
+</tr>
+
+
+<tr>
+    <td> Date of Birth: </td>
+    <td> <input type ="Date" ></td>
+
+</tr>
+
+<tr>
+    <td> Gender: </td>
+    <td><input type = "radio" name ="des" value =""> Male </td>
+    <td><input type = "radio" name ="des" value =""> Female </td>
+    <td><input type = "radio" name ="des" value =""> Other </td>
+
+</tr>
+
+<tr>
+    <td> Marital Status: </td>
+    <td>
+        <select name= "Select">
+        <option value ="text" > Select</option>
+        </Select>
+   
+
+</tr>
+
+<tr>
+    <td> Account Type: </td>
+    <td>
+        <select name= "Select">
+        <option value ="text" > Select</option>
+        </Select>
+   
+
+</tr>
+
+<tr> 
+    <td> Initial Deposit Amount: </td>
+    <td><input type ="text"></td>
+
+</tr>
+
+<tr> 
+    <td> Mobile Number: </td>
+    <td><input type ="text"></td>
+
+</tr>
+
+<tr>
+
+
+    <td> Email Address:  </td>
+    <td><input type ="text"></td>
+    
+</tr>
+
+<tr>
+
+
+    <td> Occupation:  </td>
+    <td><input type ="text"></td>
+    
+</tr>
+
+<tr>
+
+
+    <td> National ID:  </td>
+    <td><input type ="text"></td>
+    
+</tr>
+
+
+<tr>
+    <td>Set Password:</td>
+    <td><input type="password" name= "password" value= "" ></td>
+
+</tr>
+
+
+
+<tr>
+    <td> Upload ID Proofs: </td>
+    <td> <input type ="file" name= "" value ="" ></td>
+
+
+</tr>
+
+
+<tr>
+    <td colspan = "2">  <input type ="checkbox" name= "Agree" >I Agree To The Conditions.</td>
+
+</tr>
+
+<tr>
+    <td> <input type ="submit" value = "Register" class= "button"></td>
+    <td> <input type ="reset" value = "Clear" class= "button"></td>
+
+</tr>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
