e28033fc88264c894aa0ac1c199ac9d313cb61e8
SabrinaAzad001 <sabrinaazad53@gmail.com>
2025-08-03 11:01:57 +0600

    Add files via upload

---
+++ Mid labtask/labtask_3/view/task3.js
@@ -0,0 +1,31 @@
+<script>
+    function handlesubmit(){
+         var fname = document.getElementById("fname").value.trim();
+         var lname = document.getElementById("lname").Value.trim();
+         var address = document.getElementById("address").Value.trim();
+         var state = document.getElementById("state").Value.trim();
+         var city = document.getElementById("city").Value.trim();
+         var phone = document.getElementById("phone").Value.trim();
+         var create password = document.getElementById("create password").Value.trim();
+         var confirm password = document.getElementById("confirm password").Value.trim();
+
+
+
+    }
+
+ if (
+    fname ==="" || lname ==="" || address ==="" || state ===""|| city ===""|| phone ===""|| email ===""||  create password ===""|| confirm password ===""||
+ )   
+
+ if ( create password !== confirm password {
+    alert ("Do not match");
+    return false;
+ }
+ ) 
+
+ if ( fname !== alphabets {
+    alert (" Only use alphabets");
+    return false;
+ }
+ ) 
+</script>
